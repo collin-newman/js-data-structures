f09a19e8e07eac86faeb9ab11720d1b88227441b
Chris_Cunningham <this.cunningham@gmail.com>
2020-11-18 13:12:30 -0700

    Functional Pattern Queue Completed except for last test

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,38 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  let counter = 0;
 
   someInstance.enqueue = function(value) {
+    //get an array of the keys in storage, find the maximum number, than add 1, and use that as the new key when storing value
+    let lastInLine = Math.max.apply(null, Object.keys(storage));
+    storage[lastInLine + 1] = value;
+
+    storage[counter] = value;
+    counter += 1;
+    console.log(storage);
+    return value;
   };
 
   someInstance.dequeue = function() {
+
+    // if (storage[0] === undefined) {
+    let keys = Object.keys(storage);
+    let firstInLine = Math.min.apply(null, keys);
+    console.log('keys', keys, 'firstInLine', firstInLine);
+    // delete storage[firstInLine];
+    // }
+
+    let returner = storage[firstInLine];
+    delete storage[firstInLine];
+    console.log('storage', storage);
+    counter === 0 ? null : counter -= 1;
+
+    return returner;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
