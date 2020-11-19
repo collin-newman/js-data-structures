f920c3be48c29a8714fbdc6c105a45e401f65657
Chris_Cunningham <this.cunningham@gmail.com>
2020-11-18 17:26:47 -0700

    Complete -queue- all patterns

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = Object.create(queueMethods);
+  instance.counter = 0;
+  instance.removalCounter = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.counter] = value;
+  this.counter += 1;
+  return value;
+};
 
+queueMethods.dequeue = function () {
+  let popped = this.storage[this.removalCounter];
+  delete this.storage[this.removalCounter];
+  this.removalCounter += 1;
+  return popped;
+};
+
+queueMethods.size = function () {
+  return this.counter - this.removalCounter < 0 ? 0 : this.counter - this.removalCounter;
+};
