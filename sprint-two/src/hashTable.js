a0859e89fb14f888acba30862ebeffec882507b5
Chris_Cunningham <this.cunningham@gmail.com>
2020-11-20 12:00:38 -0700

    Hash Table tests complete, part 1

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (!Array.isArray(this._storage.get(index))) {
+    this._storage.set(index, []);
+  }
+  for (let i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = v;
+      return;
+    }
+  }
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+
+  if (Array.isArray(this._storage.get(index))) {
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i].includes(k)) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //this._storage.set(index, null);
+  this._storage.set(index, undefined);
 };
 
 
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -22,11 +22,16 @@ var LimitedArray = function(limit) {
   limitedArray.set = function(index, value) {
     checkLimit(index);
     storage[index] = value;
+    console.log(storage);
   };
   limitedArray.each = function(callback) {
     for (var i = 0; i < storage.length; i++) {
       callback(storage[i], i, storage);
     }
+  };
+
+  limitedArray.storageLogger = function() {
+    console.log(storage);
   };
 
   var checkLimit = function(index) {
