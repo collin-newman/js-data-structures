82c9a55a424ac6ee474a7d06949a8cd12f2ff6de
Chris_Cunningham <this.cunningham@gmail.com>
2020-11-26 15:45:00 -0700

    remove comments from .rebalance BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -187,20 +187,6 @@ binarySearchTreeMethods.rebalance = function() {
       greaterThanRoot.push(value);
     }
   });
-  //lessThanRoot => [0,1,2,3,4,5,6,7,8,9]
-  //greaterThanRoot => [11,12,13,14,15,16,17,18,19,20]
-  //get halfway point of lessThan
-  //insert on root
-  //get halfway point of greaterThan
-  //insert on root
-
-  //if 'i' is even,
-  //insert greaterThanRoot[ Math.floor(greaterThanRoot.length / 4) ]
-  //insert lessThanRoot[ Math.floor(lessThanRoot.length / 4) ]
-
-  //if 'i' is odd,
-  //insert greaterThanRoot[ Math.floor(greaterThanRoot.length * 3/4)]
-  //insert lessThanRoot[ Math.floor(greaterThanRoot.length * 3/4)]
 
   lessThanRoot.sort(function(a, b) {
     return b - a;
@@ -259,26 +245,4 @@ binarySearchTreeMethods.rebalance = function() {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-let binarySearchTree = BinarySearchTree(10);
-binarySearchTree.insert(9);
-binarySearchTree.insert(8);
-binarySearchTree.insert(7);
-binarySearchTree.insert(6);
-binarySearchTree.insert(5);
-binarySearchTree.insert(4);
-binarySearchTree.insert(3);
-binarySearchTree.insert(2);
-binarySearchTree.insert(1);
-binarySearchTree.insert(0);
-// binarySearchTree.insert(11);
-// binarySearchTree.insert(12);
-// binarySearchTree.insert(13);
-// binarySearchTree.insert(14);
-// binarySearchTree.insert(15);
-// binarySearchTree.insert(16);
-// binarySearchTree.insert(17);
-// binarySearchTree.insert(18);
-// binarySearchTree.insert(19);
-// binarySearchTree.insert(20);
 
-// binarySearchTree.rebalance();
